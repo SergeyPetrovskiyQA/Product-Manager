7b90b89749688f75c5b9b8f4f94a39384973e7e4
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-01-19 23:50:10 +0300

    change pom

--- src/main/java/ru/netology/Smartphone.java
+++ src/main/java/ru/netology/Smartphone.java
@@ -7,4 +7,12 @@ public class Smartphone extends Product {
         super(id, name, price);
         this.manufacturer = manufacturer;
     }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
